9802ddb74f35408978443142cceac72b8f92221f
lucasnunesmendes12@gmail.com <lucasnunesmendes12@gmail.com>
2025-09-16 12:03:58 -0300

    Teste

--- operacional/js/diaria.js
+++ operacional/js/diaria.js
@@ -156,7 +156,7 @@ window.addEventListener('DOMContentLoaded', () => {
         carregarAgendaDiaria(dataSelecionada, profissionalSelecionado);
     });
 
-    fetch('../operacional/sidebar.html')
+    fetch('../sidebar.html')
         .then(res => res.text())
         .then(html => {
             document.getElementById('sidebarContainer').innerHTML = html;
